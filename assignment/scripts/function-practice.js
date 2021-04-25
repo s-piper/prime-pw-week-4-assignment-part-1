9742e3c67c35e6309833f3ff68eb7d6ff2a83c62
Stephen Piper <stephen.r.piper@gmail.com>
2021-04-25 14:27:54 -0500

    completed stretch goal 8

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -99,7 +99,12 @@ console.log('Finding color purple, expecting false:', find('purple', arrayColor)
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if (letter === string.charAt (0)) {
+    return true;
+  }
+  if (letter !== string.charAt (0)) {
+    return false;
+  }
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );

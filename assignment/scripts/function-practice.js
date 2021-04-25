703a66f05969f7dd1d44bea79e90dc3f63693b40
Stephen Piper <stephen.r.piper@gmail.com>
2021-04-25 14:36:44 -0500

    finished stretch goal 9

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -110,12 +110,16 @@ console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll( array ) {
   let sum = 0
   // TODO: loop to add items
+  for (var i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
   return sum;
 }
 
+console.log('Expect to equal 6. Answer:',sumAll([1, 2, 3]));
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
